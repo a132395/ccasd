89ffed115d6eadb34f7a6faa5856044dbdeb1025
a132395 <67187730+a132395@users.noreply.github.com>
2020-12-05 16:34:21 +0800

    Update init_db.js

--- init_db.js
+++ init_db.js
@@ -58,20 +58,18 @@ async function main(){
         url VARCHAR(10000) REFERENCES posts(url)
     );`);
 
-    let respone = await gotInstance.get('forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page=1');
+    let respone = await gotInstance.get('forum.php?mod=forumdisplay&fid=103&page=1');
     const html = respone.body;
     const $ = cheerio.load(html);
     // console.log($("tbody[id^='normalthread']").length);
     const lastPageHref = $("div.pg > a.last").attr('href');
     response = await gotInstance.get(lastPageHref);
     console.log(lastPageHref)
-    //const regex = /(forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page=)(\d*).html/;
+    const regex = /(forum-103-)(\d*).html/;
     //console.log(regex);
-    //const match = lastPageHref.match(regex);
-    //const forumPrefix = match[1];
-    //const maxPageNumber = parseInt(match[2]);
-    const forumPrefix = 'forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page='
-    const maxPageNumber = 43
+    const match = lastPageHref.match(regex);
+    const forumPrefix = match[1];
+    const maxPageNumber = parseInt(match[2]);
 
     console.log('Checking new posts...');
     const query_text = `INSERT INTO posts (url,title,postdate,downloaded)
