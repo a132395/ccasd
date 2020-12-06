f048c7033ebf946079219a2af1480e61a22c2d5c
a132395 <67187730+a132395@users.noreply.github.com>
2020-12-06 16:43:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -250,7 +250,6 @@ async function main(){
         LIMIT 1;`;
         let result = await client.query(get_date_query);
         if (result==null|result.isEmpty()){
-	    const latestDate = result.rows[0].postdate;
 	    const get_new_date_query = `SELECT postdate FROM posts
             ORDER BY postdate DESC
             LIMIT 1;`;
